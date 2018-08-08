e78ce36097d874259b3cd84bc60de9095f92ad59
joseph tetteh <joemainland@josephs-macbook-pro.local>
2018-08-08 17:45:14 +0800

    Initial Commit

---
+++ starterxcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  starterxcode
+//
+//  Created by joseph tetteh on 8/8/18.
+//  Copyright © 2018 joseph tetteh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
