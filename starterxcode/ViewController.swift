02d8896d6e275d0c82ce4adb760ccb13ba451049
niifio <josephniifio@yahoo.com>
2018-08-09 13:39:29 +0800

    added a tableview and working on the 2nd screen

--- starterxcode/ViewController.swift
+++ starterxcode/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var thecooltableview: UITableView!
+    
+    var emojis = ["😀","😘","😇","😍","😜","😎","🤪","🤓","🧐","😚"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        thecooltableview.dataSource = self
+        thecooltableview.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis [indexPath.row]
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
